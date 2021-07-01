db346986ad2d1908717919792cbc71adc8530459
douqiting634 <>
2021-07-01 19:00:45 +0800

    前端页面初始化

--- website/api-test-spa/src/App.js
+++ website/api-test-spa/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from "react";
+import { Tag } from 'antd';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link,
+  // Redirect,
+  // useHistory,
+  // useLocation
+} from "react-router-dom";
 import './App.css';
+
+import { getRandomColor } from './utils'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App wh-full">
+      <Router>
+        <div className="home">
+          <div className="title text-center">前后端测试</div>
+          <ul className="mt40">
+            <li><Tag className="ft24 my-tag text-center" color={getRandomColor()}>跨域请求</Tag></li>
+          </ul>
+        </div>
+        <Switch>
+            <Route path="/public">
+              {/* <PublicPage /> */}
+            </Route>
+            <Route path="/login">
+              {/* <LoginPage /> */}
+            </Route>
+            {/* <PrivateRoute path="/protected">
+              <ProtectedPage />
+            </PrivateRoute> */}
+          </Switch>
+      </Router>
     </div>
   );
 }
